448011ccca8ff9867f65d4187cee2deb0f0d05bc
Belloshifawu <shifawubello6@gmail.com>
2023-09-05 14:46:43 -0700

    a function that copies a string

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,5 +13,6 @@ void print_buffer(char *b, int size);
 char *_strcat(char *dest, char *src);
 int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 
 #endif
