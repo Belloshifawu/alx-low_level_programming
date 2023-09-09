e684edb9210d7be17bca8d42d57bba099ee2bf6c
Belloshifawu <shifawubello6@gmail.com>
2023-09-08 19:24:10 -0700

    a function that allocates memory for an array, using malloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,5 +6,6 @@ int _strlen(char *s);
 void errors(void);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int *array_range(int min, int max);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif
