e1da6ec38ad067248f208640994774bd02fbce99
Belloshifawu <shifawubello6@gmail.com>
2023-09-22 18:39:18 -0700

    a function that prints a sentences before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+void ComeFirstbyShifawu(void)__attribute__ ((constructor));
+/**
+ * ComeFirstbyShifawu - Function that runs beforee the  main function
+ */
+void ComeFirstbyShifawu(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -25,5 +25,6 @@ int len(const char *str);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void ComeFirstbyShifawu(void);
 
 #endif
